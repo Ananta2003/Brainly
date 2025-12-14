71ae007ab3a383fc1c7468828474506b8d348e0a
Ananta <anantavigave@gmail.com>
2025-12-15 03:35:07 +0530

    hii added change

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import userRouter from './routes/user.js'
 import cors from 'cors'
+import path from "path";
 
 dotenv.config()
 
@@ -24,3 +25,9 @@ app.use('/api/v1' ,userRouter)
 app.listen(port,()=>{
     console.log("Server Running on "+port)
 })
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
