2548aaa6589f42198bdb32a3bddd3aaad799d37c
Ananta <anantavigave@gmail.com>
2025-12-17 19:17:14 +0530

    removed crap

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,6 @@ import express from 'express'
 import dotenv from 'dotenv'
 import userRouter from './routes/user.js'
 import cors from 'cors'
-import path from "path";
 
 dotenv.config()
 
@@ -25,9 +24,3 @@ app.use('/api/v1' ,userRouter)
 app.listen(port,()=>{
     console.log("Server Running on "+port)
 })
-
-app.use(express.static(path.join(__dirname, "dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
