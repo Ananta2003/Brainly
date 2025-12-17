3403001436ac433a463880757a5e4c2b78a227b4
Ananta <anantavigave@gmail.com>
2025-12-17 19:40:22 +0530

    Added api/v1

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ const port = process.env.PORT
 
 app.use(
   cors({
-    origin: ["https://brainly-frontend-22lw.onrender.com/"],
+    origin: ["https://brainly-frontend-22lw.onrender.com/api/v1/"],
     credentials: true,
   })
 );
