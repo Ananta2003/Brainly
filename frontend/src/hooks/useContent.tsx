a44c920008e301c45c59d47fbb8a8bb62c3a7d46
Ananta <anantavigave@gmail.com>
2025-12-19 04:28:54 +0530

    updated hook

--- frontend/src/hooks/useContent.tsx
+++ frontend/src/hooks/useContent.tsx
@@ -1,13 +1,15 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const token = localStorage.getItem('token')
 
 export function useContent(){
 
     const [content, setcontent]= useState<any[]>([])
 
     useEffect(()=>{
+        const token = localStorage.getItem('token')
+
+        
         axios.get('/bulk', {
             headers: {
                 Authorization: `Bearer ${token}`
